98559c20ed280b2bdace424b4f68515fbe1a8911
Luiz Gustavo <luizg.ss36@gmail.com>
2022-10-27 21:26:05 -0300

    Salvando no localstorage

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,6 +15,23 @@ export default class Main extends Component {
     index: -1,
   };
 
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (!tarefas) return;
+
+    this.setState({ tarefas });
+  }
+
+  // CAPTURA TODAS AS ATUALIZAÇÕES DE TAREFAS E JOGA NO LOCALSTORAGE
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if (tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     const { tarefas, index } = this.state;
